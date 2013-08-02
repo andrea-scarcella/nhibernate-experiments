b8a5c8606184597730bcc8f0879a3de8365ee716
Andrea Scarcella <andrea.scarcella.as@gmail.com>
2013-08-03 00:43:10 +0200

    Green tests, no db created

---
+++ FirstSolution/FirstSolution.Tests/GenerateSchema_Fixture .cs
@@ -0,0 +1,22 @@
+﻿using FirstSolution.Domain;
+using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
+using NUnit.Framework;
+
+namespace FirstSolution.Tests
+{
+    [TestFixture]
+    public class GenerateSchema_Fixture
+    {
+        [Test]
+        public void Can_generate_schema()
+        {
+            var cfg = new Configuration();
+            cfg.Configure();
+            cfg.AddAssembly(typeof(Product).Assembly);
+
+         new   SchemaExport(cfg).Execute(true, true, false);
+            //(false, true, false, false);
+        }
+    }
+}
---
+++ FirstSolution/FirstSolution/Domain/Product.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FirstSolution.Domain
+{
+    public class Product
+    {
+        public String Category
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public Boolean Discontinued
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    }
+}
