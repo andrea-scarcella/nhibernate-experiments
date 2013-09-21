c2f37645fddc81f9ae1aaf668aa7b98089dcfe11
Andrea Scarcella <andrea.scarcella.as@gmail.com>
2013-09-21 19:51:38 +0200

    Completed setting up a base entity class

--- NHibernateCookbook_Chapter1/NHibernateCookbook_Chapter1/Entity.cs
+++ NHibernateCookbook_Chapter1/NHibernateCookbook_Chapter1/Entity.cs
@@ -2,8 +2,57 @@
 
 namespace Eg.Core
 {
-	public abstract class Entity
+	public abstract class Entity<TId>
 	{
-		public virtual Guid Id { get; protected set; }
+
+		public virtual TId Id { get; protected set; }
+
+		public override bool Equals(object obj)
+		{
+			return Equals(obj as Entity<TId>);
+		}
+
+		private static bool IsTransient(Entity<TId> obj)
+		{
+			return obj != null &&
+				   Equals(obj.Id, default(TId));
+		}
+
+		private Type GetUnproxiedType()
+		{
+			return GetType();//hier wordt  de methode van de parent class gebruikt
+		}
+
+		public virtual bool Equals(Entity<TId> other)
+		{
+			if (other == null)
+				return false;
+
+			if (ReferenceEquals(this, other))
+				return true;
+
+			if (!IsTransient(this) &&
+				!IsTransient(other) &&
+				Equals(Id, other.Id))
+			{
+				var otherType = other.GetUnproxiedType();
+				var thisType = GetUnproxiedType();
+				return thisType.IsAssignableFrom(otherType) ||
+					   otherType.IsAssignableFrom(thisType);
+			}
+
+			return false;
+		}
+
+		public override int GetHashCode()
+		{
+			if (Equals(Id, default(TId)))
+				return base.GetHashCode();//???
+			return Id.GetHashCode();
+		}
+
+	}
+	public abstract class Entity : Entity<Guid>
+	{
 	}
 }
