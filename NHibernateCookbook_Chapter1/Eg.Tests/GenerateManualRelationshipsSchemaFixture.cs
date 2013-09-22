e260304e51c097383f3b8861365a26920a7ffff1
Andrea Scarcella <andrea.scarcella.as@gmail.com>
2013-09-22 12:53:48 +0200

    Adde manual relationships example

---
+++ NHibernateCookbook_Chapter1/Eg.Tests/GenerateManualRelationshipsSchemaFixture.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ManualRelationships;
+using NHibernate.Cfg;
+using NUnit.Framework;
+namespace Eg.Tests
+{
+	public class GenerateManualRelationshipsSchemaFixture : TestBase
+	{
+		[SetUp]
+		public void Setup()
+		{
+			Cfg = new Configuration();
+			Cfg.Configure();
+			Cfg.AddAssembly(typeof(Order).Assembly);
+		}
+	}
+}
---
+++ NHibernateCookbook_Chapter1/ManualRelationships/Order.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Iesi.Collections.Generic;
+
+namespace ManualRelationships
+{
+	public class Order
+	{
+		private HashedSet<OrderItem> _items;
+		public virtual Guid Id { get; protected set; }
+
+		public Order()
+		{
+			_items = new HashedSet<OrderItem>();
+		}
+		public virtual IEnumerable<OrderItem> Items
+		{
+			get
+			{
+				return _items;
+			}
+		}
+		public virtual bool AddItem(OrderItem newItem)
+		{
+			if (newItem != null && _items.Add(newItem))
+			{
+				newItem.SetOrder(this);
+				return true;
+			}
+			return false;
+		}
+
+		public virtual bool RemoveItem(
+		  OrderItem itemToRemove)
+		{
+			if (itemToRemove != null &&
+			  _items.Remove(itemToRemove))
+			{
+				itemToRemove.SetOrder(null);
+				return true;
+			}
+			return false;
+		}
+
+	}
+
+	
+}
