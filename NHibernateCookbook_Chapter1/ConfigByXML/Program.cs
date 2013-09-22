df86ce78f76ec52537ba169684fdc067e1013120
Andrea Scarcella <andrea.scarcella.as@gmail.com>
2013-09-22 17:20:27 +0200

    Added Configuration by Xml example

---
+++ NHibernateCookbook_Chapter1/ConfigByXML/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NHibernate.Cfg;
+
+namespace ConfigByXML
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var nhConfig = new Configuration().Configure();
+			var sessionFactory = nhConfig.BuildSessionFactory();
+			Console.WriteLine("NHibernate Configured!");
+			ConsoleKeyInfo consoleKeyInfo = Console.ReadKey();
+		}
+	}
+}
