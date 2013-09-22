b739b8281b37b0271112b3d88a0c0c3b74664ca8
Andrea Scarcella <andrea.scarcella.as@gmail.com>
2013-09-22 18:46:59 +0200

    Stubbed this example

---
+++ NHibernateCookbook_Chapter1/ConfigWithConfORM/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NHibernate.Cfg;
+using NHibernate.Dialect;
+
+namespace ConfigWithConfORM
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Could not get it to compile
+			//Could not resolve symbol 'MappingFactory'
+			//Shelved it for now
+
+		}
+	}
+}
---
+++ NHibernateCookbook_Chapter1/ConfigWithConfORM/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ConfigWithConfORM")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConfigWithConfORM")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("682fdc43-c812-4e7e-bba1-3c7afe0e2995")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
