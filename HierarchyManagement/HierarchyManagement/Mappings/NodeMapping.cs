0cf44b8d7b3af2aae8ca02415a5a25f49c9de754
Andrea Scarcella <andrea.scarcella.as@gmail.com>
2013-09-28 01:16:30 +0200

    First version

---
+++ HierarchyManagement/HierarchyManagement/Mappings/NodeMapping.cs
@@ -0,0 +1,18 @@
+﻿using FluentNHibernate.Mapping;
+
+namespace HierarchyManagement.Mappings
+{
+	public class NodeMapping : ClassMap<Node>
+	{
+		public NodeMapping()
+		{
+			Id(n => n.Id).GeneratedBy.GuidComb();
+			Map(n => n.Name).Length(50).Not.Nullable();
+			Map(n => n.Parent);//????
+			References(n => n.Parent).Not.Nullable();//many2one
+			HasManyToMany(n => n.Ancestors).Inverse().Cascade.All().Table("NodeHierarchy").AsSet();
+			HasMany(n => n.Children).Inverse().Cascade.All().KeyColumn("ParentId").AsSet();//one2many
+			HasManyToMany(n => n.Descendants).Inverse().Cascade.All().Table("NodeHierarchy").AsSet();
+		}
+	}
+}
---
+++ HierarchyManagement/HierarchyManagement/Node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HierarchyManagement
+{
+	public class Node
+	{
+		public virtual Guid Id { get; set; }
+		public virtual string Name { get; set; }
+		public virtual Node Parent { get; set; }
+		public virtual ISet<Node> Children { get; set; }
+
+		public virtual ISet<Node> Ancestors { get; set; }
+		public virtual ISet<Node> Descendants { get; set; }
+	}
+
+}
